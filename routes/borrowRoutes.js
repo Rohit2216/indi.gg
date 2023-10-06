7857c656183b884c57bc5a887aefd113e06c8059
Rohit Chauhan <117335464+Rohit2216@users.noreply.github.com>
2023-10-06 19:44:17 +0530

    Update borrowRoutes.js

--- routes/borrowRoutes.js
+++ routes/borrowRoutes.js
@@ -5,5 +5,6 @@ const {Authentication}=require("../middlewares/authentication")
 
 borrowRouter.post('/borrowbooks',Authentication(["admin","user"]), borrowedBookController.borrowBook);
 borrowRouter.post('/returnbooks',Authentication(["admin","user"]), borrowedBookController.returnBook);
+borrowRouter.get('/user/:userId',Authentication(["admin","user"]), borrowedBookController.getBorrowedBooksByUserId);
 
 module.exports = {borrowRouter};
