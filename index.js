5f8fc08f7d4d065cce40463b5d872bf392533d8e
Rohit Chauhan <117335464+Rohit2216@users.noreply.github.com>
2023-10-06 18:40:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const {recommendRouter}=require("./routes/recommendationRoute")
 
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-
+const cors = require("cors")
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.get("/",(req,res)=>{
     res.status(200).send({ "msg": "Welcome to Indi.gg" })
