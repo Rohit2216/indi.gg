8fdab2f3c1cb1fd8c2f8911aab2fa85840ce12ea
Rohit2216 <chauhanrohit716@gmail.com>
2023-10-06 18:29:32 +0530

    done

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ const swaggerOptions = {
     definition: {
       openapi: '3.0.0',
       info: {
-        title: 'Your API Documentation',
+        title: 'Library Management System API Documentation',
         version: '1.0.0',
-        description: 'API documentation for your application',
+        description: 'API documentation for Library Management System application',
       },
       servers: [
         {
